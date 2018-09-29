d68910d40d9d5d6e38c6204f540b7bd5435f08de
JayasriTanneru402 <41224487+JayasriTanneru402@users.noreply.github.com>
2018-09-29 09:46:17 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-virtualenv -p python3 myenvs
-. myenvs/bin/activate
-pip install django pillow coverage django-jenkins 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py jenkins --enable-coverage
