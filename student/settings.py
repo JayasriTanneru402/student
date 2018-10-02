b28a9e7d79fa52e20bbb4bd569533292f63df13b
JayasriTanneru402 <jayasri.tanneru402@gmail.com>
2018-10-02 08:23:11 +0530

    pep8 standards

--- student/settings.py
+++ student/settings.py
@@ -46,8 +46,7 @@ INSTALLED_APPS = PREREQ_APPS + PROJECT_APPS
 
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-    
+
 )
 
 
--- studentapp/views.py
+++ studentapp/views.py
@@ -6,15 +6,16 @@ from django.shortcuts import redirect
 
 # Create your views here.
 
+
 class Home(View):
 
     def get(self, request):
         sform = StudentForm()
         data = Student.objects.all()
-        return render(request, 'home.html', {'form':sform, 'data': data})
+        return render(request, 'home.html', {'form': sform, 'data': data})
 
     def post(self, request):
-        sdata = StudentForm(self.request.POST,self.request.FILES)
+        sdata = StudentForm(self.request.POST, self.request.FILES)
         if sdata.is_valid():
             sdata.save()
             return redirect('/')
